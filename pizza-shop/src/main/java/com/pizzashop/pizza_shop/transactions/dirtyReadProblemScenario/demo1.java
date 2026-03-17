bcaa0405b22ae63e89d85e72824ee7328d7a6231
madhan <heyitsmadhan@gmail.com>
2026-03-17 23:22:16 +0530

    Implemented how dirty read occures

---
+++ pizza-shop/src/main/java/com/pizzashop/pizza_shop/transactions/dirtyReadProblemScenario/demo1.java
@@ -0,0 +1,45 @@
+package com.pizzashop.pizza_shop.transactions.dirtyReadProblemScenario;
+
+
+import com.pizzashop.pizza_shop.entity.Pizza;
+import com.pizzashop.pizza_shop.repository.PizzaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class demo1 {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    private static Logger logger = LoggerFactory.getLogger(demo1.class);
+
+    @Transactional(isolation= Isolation.READ_UNCOMMITTED)
+    public void transactionA() throws Exception {
+        logger.info("transaction A reading the total pizzas available quantity: {} ", pizzaRepository.totalStock());
+        Pizza pizza = pizzaRepository.findById(1L).orElseThrow();
+        logger.info("Pizza Name: {} and stock: {} ", pizza.getName(), pizza.getStock());
+        pizza.setStock(0);
+        pizzaRepository.save(pizza); //we are saving pizza table but commit is not going to happen here as
+        // at the method end commit will get executed and if no exception is thrown
+        logger.info("Pizza Name: {} and updated stock: {} ", pizza.getName(), pizza.getStock());
+        logger.info("Transaction A updated stock to 0 but NOT committed");
+        pizzaRepository.flush(); // force hibernate to push the changes immedialty to DB
+
+
+        Thread.sleep(10000);// this keeps transaction open for 10 seconds
+        throw new RuntimeException("An exception occurred in transaction A roll back!");
+    }
+
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
+    public void transactionB()
+    {
+        logger.info("transaction B reading the total pizzas available quantity: {} ", pizzaRepository.totalStock());
+        Pizza pizza = pizzaRepository.findById(1L).orElseThrow();
+        logger.info("Transaction B: Pizza Name: {} and stock: {} ", pizza.getName(), pizza.getStock());
+    }
+}
