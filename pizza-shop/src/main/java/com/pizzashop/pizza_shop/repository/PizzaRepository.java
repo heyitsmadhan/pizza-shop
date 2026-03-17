d05535446749f9839fe84bb96562c6c48d98dab7
madhan <heyitsmadhan@gmail.com>
2026-03-17 23:31:09 +0530

    included command line runner

--- pizza-shop/src/main/java/com/pizzashop/pizza_shop/repository/PizzaRepository.java
+++ pizza-shop/src/main/java/com/pizzashop/pizza_shop/repository/PizzaRepository.java
@@ -2,8 +2,12 @@ package com.pizzashop.pizza_shop.repository;
 
 import com.pizzashop.pizza_shop.entity.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza,Long> {
+
+    @Query("SELECT SUM(stock) FROM Pizza")
+    public int totalStock();
 }
